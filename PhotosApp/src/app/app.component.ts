ff20450d59cd814270ff01b9af7cfa5c6f090406
William Pereira <wil.scp@gmail.com>
2021-11-15 23:31:36 -0300

    Create App Angular

---
+++ PhotosApp/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ph-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'PhotosApp';
+}
